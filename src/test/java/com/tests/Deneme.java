b9977e551f3ab1ddb498f4b2589ceeae050622ce
esra balci <esrambalci@hotmail.com>
2022-05-17 21:24:59 -0400

    day 1 is finished

--- src/test/java/com/tests/Deneme.java
+++ src/test/java/com/tests/Deneme.java
@@ -1,4 +1,10 @@
 package com.tests;
 
+import com.google.gson.internal.bind.util.ISO8601Utils;
+
 public class Deneme {
+
+    public static void main(String[] args) {
+        System.out.println("first day is done");
+    }
 }
